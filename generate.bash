71b7d1270a3f77430de7aa80fd24bbfd2d18cd8e
goronu <maxime@durrmail.com>
2024-12-11 22:00:55 +0000

    Update to version 1.41.3.9292

--- generate.bash
+++ generate.bash
@@ -1,85 +1,74 @@
 #!/bin/bash
 
+# Configuration
 api="https://plex.tv/pms/downloads/5.json"
 fileversion="./lastversion.txt"
+repo_xml="./repo/repo.xml"
+repo_dir="/opt/Unofficial-QNAP-Plex-Repo"
+branch="main"
+github_token="YOUR_GITHUB_TOKEN"
+github_repo="https://$github_token@github.com/your_user/your_repo.git"
+
+# Fetch the current version
 lastversion=$(cat $fileversion)
 version=$(curl -s $api | jq -r '.nas.QNAP.version')
 shortversion=${version:0:-10}
 date=$(curl -s $api | jq -r '.nas.QNAP.release_date')
 published=$(date -d @$date -u +'%Y/%m/%d')
 cache=$(date +'%Y%m%d%H%M%S')
-repo_xml="./repo/repo.xml"
 
-cd /opt/Unofficial-QNAP-Plex-Repo
+cd $repo_dir || { echo "Error: Cannot access directory $repo_dir"; exit 1; }
 
-if [[ "$lastversion" == "$version" ]]
-then
-        echo "The version is the same"
+# Check if the version has changed
+if [[ "$lastversion" == "$version" ]]; then
+    echo "The version is the same, no update needed."
+    exit 0
+fi
+
+# Generate the XML file
+echo "The version is different, updating..."
+{
+    echo "<?xml version='1.0' encoding='utf-8'?>"
+    echo "<plugins>"
+    echo "  <cachechk>$cache</cachechk>"
+    echo "  <item>"
+    echo "    <name>Plex Media Server</name>"
+    echo "    <internalName>Plex Media Server</internalName>"
+    echo "    <category>Essentials</category>"
+    echo "    <type>Entertainment</type>"
+    echo "    <icon80>https://download.qnap.com/QPKG/images/QPKG/plex_80.png</icon80>"
+    echo "    <icon100>https://download.qnap.com/QPKG/images/QPKG/plex_80.png</icon100>"
+    echo "    <description>Plex organizes all of your personal media so you can easily access and enjoy it. Notice: If you upgrade the QNAP firmware and the app stops working, please reinstall the app.</description>"
+    echo "    <fwVersion>4.3.0</fwVersion>"
+    echo "    <version>$shortversion</version>"
+    echo "    <platform>"
+    echo "      <platformID>TS-NASX86</platformID>"
+    echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-x86_64.qpkg</location>"
+    echo "    </platform>"
+    echo "    <platform>"
+    echo "      <platformID>TS-NASARM_64</platformID>"
+    echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-aarch64.qpkg</location>"
+    echo "    </platform>"
+    echo "    <publishedDate>$published</publishedDate>"
+    echo "    <maintainer>Plex</maintainer>"
+    echo "    <forumLink>https://plex.tv</forumLink>"
+    echo "    <snapshot>https://download.qnap.com/QPKG/img/PlexQNAPScreenshot.png</snapshot>"
+    echo "    <tutorialLink>https://support.plex.tv/articles/200264746-quick-start-step-by-step-guides/#setting-up-and-installing-the-plex-media-server</tutorialLink>"
+    echo "    <forumLink>https://forums.plex.tv/</forumLink>"
+    echo "  </item>"
+    echo "</plugins>"
+} > $repo_xml
+
+# Save the new version
+echo $version > $fileversion
+
+# Add, commit, and push changes to GitHub
+echo "Pushing changes to GitHub..."
+git add *
+git commit -m "Update to version $shortversion"
+if git push origin $branch; then
+    echo "Changes successfully pushed to GitHub."
 else
-        echo "La version is different"
-        echo "<?xml version='1.0' encoding='utf-8'?>" > $repo_xml
-        echo "<plugins>" >> $repo_xml
-        echo "  <cachechk>$cache</cachechk>" >> $repo_xml
-        echo "  <item>" >> $repo_xml
-        echo "    <name>Plex Media Server</name>" >> $repo_xml
-        echo "    <internalName>Plex Media Server</internalName>" >> $repo_xml
-        echo "    <category>Essentials</category>" >> $repo_xml
-        echo "    <type>Entertainment</type>" >> $repo_xml
-        echo "    <icon80>https://download.qnap.com/QPKG/images/QPKG/plex_80.png</icon80>" >> $repo_xml
-        echo "    <icon100>https://download.qnap.com/QPKG/images/QPKG/plex_80.png</icon100>" >> $repo_xml
-        echo "    <description>Plex organizes all of your personal media so you can easily access and enjoy it. Notice: If you upgrade the QNAP firmware and the app stops working, please reinstall the app.</description>" >> $repo_xml
-        echo "    <fwVersion>4.3.0</fwVersion>" >> $repo_xml
-        echo "    <version>$shortversion</version>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-NASX86</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-x86_64.qpkg</location>" >> $repo_xml
-        echo "    </platform>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-NASARM_64</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-aarch64.qpkg</location>" >> $repo_xml
-        echo "    </platform>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-X31P</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-armv7neon.qpkg</location>" >> $repo_xml
-        echo "    </platform>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-X31P2</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-armv7neon.qpkg</location>" >> $repo_xml
-        echo "    </platform>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-X31X</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-armv7neon.qpkg</location>" >> $repo_xml
-        echo "    </platform>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-X31XU</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-armv7neon.qpkg</location>" >> $repo_xml
-        echo "    </platform>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-X31</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-armv7hf.qpkg</location>" >> $repo_xml
-        echo "    </platform>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-X31U</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-armv7hf.qpkg</location>" >> $repo_xml
-        echo "    </platform>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-X28</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-aarch64.qpkg</location>" >> $repo_xml
-        echo "    </platform>" >> $repo_xml
-        echo "    <platform>" >> $repo_xml
-        echo "      <platformID>TS-X32</platformID>" >> $repo_xml
-        echo "      <location>https://downloads.plex.tv/plex-media-server-new/$version/qnap/PlexMediaServer-$version-aarch64.qpkg</location>" >> $repo_xml
-        echo "     </platform>" >> $repo_xml
-        echo "    <publishedDate>$published</publishedDate>" >> $repo_xml
-        echo "    <maintainer>Plex</maintainer>" >> $repo_xml
-        echo "    <forumLink>https://plex.tv</forumLink>" >> $repo_xml
-        echo "    <snapshot>https://download.qnap.com/QPKG/img/PlexQNAPScreenshot.png</snapshot>" >> $repo_xml
-        echo "    <tutorialLink>https://support.plex.tv/articles/200264746-quick-start-step-by-step-guides/#setting-up-and-installing-the-plex-media-server</tutorialLink>" >> $repo_xml
-        echo "    <forumLink>https://forums.plex.tv/</forumLink>" >> $repo_xml
-        echo "  </item>" >> $repo_xml
-        echo "</plugins>" >> $repo_xml
-        echo $version > $fileversion
-	git add *
-	git commit -m "Update" -a
-	git push origin main
+    echo "Error pushing to GitHub. Check your token or permissions."
+    exit 1
 fi
